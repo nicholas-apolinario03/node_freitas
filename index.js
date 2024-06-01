69eaca86cb079a677baeb344edb2b09ce2961b47
nicholas-apolinario03 <50881682+nicholas-apolinario03@users.noreply.github.com>
2024-05-31 22:14:26 -0300

    Update index.js

--- index.js
+++ index.js
@@ -43,9 +43,15 @@ async function startServer() {
                 res.status(500).json({ error: 'Erro ao buscar dados no MongoDB' });
             }
         });
-        app.use((req, res) => {
-          res.sendFile(__dirname + '/index.html');
+        app.use((req, res, next) => {
+          // Verifica se a rota solicitada corresponde a uma página existente
+          if (req.url === '/' || req.url === '/index.html') {
+              res.sendFile(__dirname + '/public' + req.url + '.html');
+          } else {
+              next();
+          }
       });
+
         const PORT = process.env.PORT || 3000;
         app.listen(PORT, () => {
             console.log("Server is running on port " + PORT);
