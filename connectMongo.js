4152dc5c9740cac94f72cccb75b4d4beb3f53e96
nicholas-apolinario03 <50881682+nicholas-apolinario03@users.noreply.github.com>
2024-05-31 21:29:51 -0300

    Update connectMongo.js

--- connectMongo.js
+++ connectMongo.js
@@ -1,12 +1,16 @@
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
+require('dotenv').config();
 
 const connectDB = async () => {
     try {
-        await mongoose.connect(process.env.MONGODB_CONNECT_URI)
-        console.log("Connect to MongoDB successfully")
+        await mongoose.connect(process.env.MONGO_URI, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        });
+        console.log("Connected to MongoDB successfully");
     } catch (error) {
-        console.log("Connect failed " + error.message )
+        console.log("Connection failed: " + error.message);
     }
-}
+};
 
-module.exports = connectDB
+module.exports = connectDB;
